0b920b3bd7282ee587744d2be4a314e0df6b6a10
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-16 17:34:40 +0300

    Add project files

--- src/build/build.sh
+++ src/build/build.sh
@@ -7,4 +7,15 @@ mysql_install_db
 
 /usr/bin/mysqld_safe &
 
+RET=1
+
+while [[ RET -ne 0 ]]
+do
+    sleep 5
+
+    mysql -uroot -e "status"
+
+    RET=$?
+done
+
 mysqladmin -u root password "root"
