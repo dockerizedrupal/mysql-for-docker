c585c39217dc9f358ce438d7d97982dbb7e9d12f
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-16 23:21:28 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+dpkg-divert --local --rename --add /sbin/initctl
+ln -s /bin/true /sbin/initctl
+
 puppet module install puppetlabs-mysql
 
 cp /opt/build/etc/puppet/manifests/site.pp /etc/puppet/manifests/site.pp
