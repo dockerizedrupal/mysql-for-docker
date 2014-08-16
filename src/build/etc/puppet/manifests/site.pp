97cf84e3cdc120e301dedf55f24486abcac6cc09
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-17 02:10:37 +0300

    Update

--- src/build/etc/puppet/manifests/site.pp
+++ src/build/etc/puppet/manifests/site.pp
@@ -3,12 +3,8 @@ node default {
     root_password => 'root',
     override_options => {
       'mysqld' => {
-        'max_connections' => '1024',
         'bind-address' => '0.0.0.0',
       },
-      'mysqld_safe' => {
-        'bind-address' => '0.0.0.0'
-      }
     }
   }
 
