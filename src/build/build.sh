fc1cbce2dcd093dfb3703c60828657fe00330f67
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-16 22:08:55 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-cat /etc/puppet/manifests/site.pp
-
-ls -l /etc/puppet/modules
-
 puppet module install puppetlabs-mysql
 
 cp /opt/build/etc/puppet/manifests/site.pp /etc/puppet/manifests/site.pp
