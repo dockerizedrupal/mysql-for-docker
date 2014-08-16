6b98adbca22f164c4790b94cfb9379a63b72b35b
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-16 21:33:52 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-apt-get update
-apt-get -y install mysql-server
-
-mysql_install_db
-
-service mysql start
-
-mysqladmin -u root password "root"
-
 ls /etc/puppet/manifests -la
 
 puppet module install puppetlabs-mysql
+
