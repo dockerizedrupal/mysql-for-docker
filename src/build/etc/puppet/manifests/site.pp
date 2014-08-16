f8dfd5b6578521aa14f7ae4211e8e93f9c48edff
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-16 22:48:10 +0300

    Update

--- src/build/etc/puppet/manifests/site.pp
+++ src/build/etc/puppet/manifests/site.pp
@@ -1,3 +1,11 @@
 node default {
-  include '::mysql::server'
+  class { '::mysql::server':
+    root_password => 'root',
+    override_options => {
+      'mysqld' => {
+        'max_connections' => '1024',
+        'bind-address' => '0.0.0.0',
+      },
+    },
+  }
 }
