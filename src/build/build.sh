8d956754157a86ba681b64b4e24e1df46acd5d5e
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-16 21:41:39 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -4,3 +4,6 @@ ls /etc/puppet/manifests -la
 
 puppet module install puppetlabs-mysql
 
+cp /opt/build/etc/puppet/manifests/mysql.pp /etc/puppet/manifests/mysql.pp
+
+puppet apply /etc/puppet/manifests/mysql.pp
