201069bb615ff1df2ea1bd44c68946b895b3c661
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-16 17:42:36 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -5,16 +5,6 @@ apt-get -y install mysql-server
 
 mysql_install_db
 
-/usr/bin/mysqld_safe &
-
-RET=1
-
-while [[ RET -ne 0 ]]; do
-    sleep 5
-
-    mysql -uroot -e "status"
-
-    RET=$?
-done
+service mysql start
 
 mysqladmin -u root password "root"
