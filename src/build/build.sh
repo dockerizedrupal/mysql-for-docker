344bcbe561ad29ddbc30a30250b470c24c749c07
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-18 19:38:27 +0300

    Update

--- src/build/build.sh
+++ src/build/build.sh
@@ -4,4 +4,6 @@ puppet module install puppetlabs-mysql
 
 puppet apply /opt/build/etc/puppet/manifests/build.pp
 
+rm /opt/build/etc/puppet/manifests/build.pp
+
 cp /opt/build/etc/puppet/manifests/run.pp /etc/puppet/manifests/run.pp
