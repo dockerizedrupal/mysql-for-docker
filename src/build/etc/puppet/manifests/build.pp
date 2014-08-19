1501c70dcee076a3a2b2497c2df240b848e0860d
Jürgen Viljaste <j.viljaste@gmail.com>
2014-08-19 08:03:19 +0300

    Update

--- src/build/etc/puppet/manifests/build.pp
+++ src/build/etc/puppet/manifests/build.pp
@@ -2,23 +2,24 @@ node default {
   file { '/opt/run.sh':
     ensure => present,
     source => '/tmp/build/run.sh',
+    mode => 755
   }
 
   file { '/etc/puppet/manifests/run.pp':
     ensure => present,
-    source => '/tmp/build/etc/puppet/manifests/run.pp',
+    source => '/tmp/build/etc/puppet/manifests/run.pp'
   }
 
   file { '/etc/supervisor/conf.d/mysql.conf':
     ensure => present,
-    source => '/tmp/build/etc/supervisor/conf.d/mysql.conf',
+    source => '/tmp/build/etc/supervisor/conf.d/mysql.conf'
   }
 
   class { 'mysql::server':
     root_password => 'root',
     override_options => {
       'mysqld' => {
-        'bind-address' => '0.0.0.0',
+        'bind-address' => '0.0.0.0'
       }
     }
   }
