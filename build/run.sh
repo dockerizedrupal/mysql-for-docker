a307c241da2bd71cc808cde6a204d61ebccac1d0
Jürgen Viljaste <j.viljaste@gmail.com>
2014-09-05 02:17:46 +0300

    Update

--- build/run.sh
+++ build/run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [ ! "$(ls -A /var/lib/mysql)" ]; then
+  /usr/bin/mysql_install_db
+
+  /etc/init.d/mysql start
+
+  echo "UPDATE mysql.user SET password = PASSWORD('root') WHERE user = 'root';" | /usr/bin/mysql
+fi
+
 #if [ -n "${MYSQL_PASSWORD}" ]; then
 #  /etc/init.d/mysql start && echo \"UPDATE mysql.user SET password = PASSWORD(\'root\') WHERE user = \'root\';\" | mysql
 #fi
