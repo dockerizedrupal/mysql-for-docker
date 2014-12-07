b27c8992b2b1d3a15d0254738e1620e4d3e45671
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 20:03:33 +0200

    Update

--- src/build/modules/mysqld/manifests/packages.pp
+++ src/build/modules/mysqld/manifests/packages.pp
@@ -7,16 +7,6 @@ class mysqld::packages {
       'mysql-server'
     ]:
     ensure => present,
-    require => Exec['apt-get update'],
-    before => Exec['apt-get clean']
-  }
-
-  exec { 'apt-get clean':
-    path => ['/usr/bin']
-  }
-
-  exec { 'rm -rf /var/lib/apt/lists':
-    path => ['/bin'],
-    require => Exec['apt-get clean']
+    require => Exec['apt-get update']
   }
 }
