77c4b9750c4814ef828816c3cde471110cc56916
Jürgen Viljaste <j.viljaste@gmail.com>
2015-09-16 17:21:40 +0300

    Update

--- src/mysql/run/modules/run/manifests/timezone.pp
+++ src/mysql/run/modules/run/manifests/timezone.pp
@@ -1,3 +1,7 @@
 class run::timezone {
-  bash_exec { "timedatectl set-timezone $timezone": }
+  bash_exec { "echo '$timezone' > /etc/timezone": }
+
+  bash_exec { 'dpkg-reconfigure -f noninteractive tzdata':
+    require => Bash_exec["echo '$timezone' > /etc/timezone"]
+  }
 }
